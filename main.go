046748ef8413805a005832e59c7339f47a3ac2ba
Eduard Generalov <eduard@generalov.net>
2022-10-31 02:07:04 +0300

    remove orphans

--- main.go
+++ main.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"os/exec"
-	"strings"
+  "encoding/json"
+  "fmt"
+  "os/exec"
+  "strings"
 )
 
 type MachineRaw struct {
-	Addresses string `json:"addresses,omitempty"`
-	Class     string `json:"class,omitempty"`
-	Machine   string `json:"machine,omitempty"`
-	Os        string `json:"os,omitempty"`
-	Service   string `json:"service,omitempty"`
-	Version   string `json:"version,omitempty"`
+  Addresses string `json:"addresses,omitempty"`
+  Class     string `json:"class,omitempty"`
+  Machine   string `json:"machine,omitempty"`
+  Os        string `json:"os,omitempty"`
+  Service   string `json:"service,omitempty"`
+  Version   string `json:"version,omitempty"`
 }
 
 type Machine struct {
-	Name      string `json:"name"`
-	Addresses string `json:"addresses"`
+  Name      string `json:"name"`
+  Addresses string `json:"addresses"`
 }
 
 func List() ([]MachineRaw, error) {
-	cmd := exec.Command("machinectl", "list", "-o", "json")
-	stdout, err := cmd.Output()
-	if err != nil {
-		return nil, err
-	}
-	var machines []MachineRaw
-	err = json.Unmarshal(stdout, &machines)
-	if err != nil {
-		return nil, err
-	}
-	return machines, nil
+  cmd := exec.Command("machinectl", "list", "-o", "json")
+  stdout, err := cmd.Output()
+  if err != nil {
+    return nil, err
+  }
+  var machines []MachineRaw
+  err = json.Unmarshal(stdout, &machines)
+  if err != nil {
+    return nil, err
+  }
+  return machines, nil
 }
 
 func MachinesRawToMachines(m []MachineRaw) []Machine {
-	var machines []Machine
-	for _, machine := range m {
-		if machine.Class != "container" || machine.Service != "systemd-nspawn" || machine.Addresses == "" {
-			continue
-		}
-		var ipv4 string
-		for _, address := range strings.Split(machine.Addresses, "\n") {
-			if strings.Count(address, ".") == 3 {
-				ipv4 = address
-				break
-			}
-		}
-		if ipv4 != "" && machine.Machine != "" {
-			machines = append(machines, Machine{
-				Addresses: ipv4,
-				Name:      machine.Machine,
-			})
-		}
-	}
-	return machines
+  var machines []Machine
+  for _, machine := range m {
+    if machine.Class != "container" || machine.Service != "systemd-nspawn" || machine.Addresses == "" {
+      continue
+    }
+    var ipv4 string
+    for _, address := range strings.Split(machine.Addresses, "\n") {
+      if strings.Count(address, ".") == 3 {
+        ipv4 = strings.Replace(address, "…", "", -1)
+        break
+      }
+    }
+    if ipv4 != "" && machine.Machine != "" {
+      machines = append(machines, Machine{
+        Addresses: ipv4,
+        Name:      machine.Machine,
+      })
+    }
+  }
+  return machines
 }
 
 func main() {
-	if raw, err := List(); err != nil {
-		panic(err)
-	} else {
-		machines := MachinesRawToMachines(raw)
-		if j, err := json.Marshal(machines); err != nil {
-			panic(err)
-		} else {
-			fmt.Println(string(j))
-		}
-	}
+  if raw, err := List(); err != nil {
+    panic(err)
+  } else {
+    machines := MachinesRawToMachines(raw)
+    if j, err := json.Marshal(machines); err != nil {
+      panic(err)
+    } else {
+      fmt.Println(string(j))
+    }
+  }
 }
